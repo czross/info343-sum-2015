e39fd176635d5a1942a349f1a4616012f1638cca
czross <czr@uw.edu>
2015-07-02 17:41:39 -0700

    1/2 way through
    half way through the class today

--- tasks/js/common.js
+++ tasks/js/common.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //initialize Parse library with your application ID and your app's JavaScript library key
-Parse.initialize('your application id', 'your app JavaScript library key');
+Parse.initialize('eGWCJfJD19HGj1Nm7s2Qbz0y0Z39mMc7lrJY4bbY', '031UBDUDabVmxUuQPk13yCAWgI7Xt6PfuHAHizqZ');
 
 /**
  * Shows an error in an element on the page with the class 'error-message'
--- tasks/js/tasks.js
+++ tasks/js/tasks.js
@@ -8,3 +8,31 @@
  */
 
 //use jQuery to register a function that is called when the document is ready for manipulation
+$(function() {
+   var currentUser = Parse.User.current();
+    if(!currentUser) {
+        window.location = 'signin.html'
+    }
+    $('.nav-link-sign-out').click(function(evt) {
+        evt.preventDefault();
+        Parse.User.logOut();
+        window.location = 'signin.html';
+    })
+
+    $('.user-name').text(currentUser.get('firstName') + ' ' + currentUser.get('lastName'));
+
+    var Task = Parse.Object.extend('Task');
+
+    var tasksQuery = new Parse.Query(Task);
+    tasksQuery.equalTo('user', currentUser);
+    tasksQuery.ascending('done, createdAt');
+
+    var TaskList = Parse.Collection.extend({
+       model: Task,
+       query: tasksQuery
+    });
+
+    var tasks = new TaskList();
+
+    tasks.fetch();
+});
